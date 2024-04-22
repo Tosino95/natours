c0c315024c530e8d03f0da62a74f27eb4d48dfed
olutosin95 <tosinonilogbo@gmail.com>
2024-04-21 22:45:29 -0500

    Implemented CORS

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ const xss = require('xss-clean');
 const hpp = require('hpp');
 const cookieParser = require('cookie-parser');
 const compression = require('compression');
+const cors = require('cors');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -29,6 +30,19 @@ app.set('views', path.join(__dirname, 'views'));
 // 1) GLOBAL MIDDLEWARE
 // This is middleware. Middleware is basically a function that can modify the incoming request data. It's called middleware because it stands between,
 // so in the middle of the request and the response. It's just a step that the request goes through while it's being processed.
+
+//Implement CORS
+app.use(cors());
+// Access-Control-Allow-Origin *
+// api.natours.com, front-end natours.com
+// app.use(
+//   cors({
+//     origin: 'https://www.natours.com',
+//   }),
+// );
+
+app.options('*', cors());
+app.options('/api/v1/tours:id', cors());
 
 // Serving static files from a folder and not a route
 // app.use(express.static(`${__dirname}/public`));
